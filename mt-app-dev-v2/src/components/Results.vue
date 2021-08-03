3832065fe42796238ef7f99a5813ed1fdf1fdd12
candidate.Brantley.Wyche.ext <bxw5869@rit.edu>
2021-08-03 13:46:36 -0400

    finished the results page

--- mt-app-dev-v2/src/components/Results.vue
+++ mt-app-dev-v2/src/components/Results.vue
@@ -1,7 +1,69 @@
 <template>
   <div class="hello">
     <b-button variant="danger" @click="findCloseBuyButtonPressed()">Show Results</b-button>
-    <div id="map" ref="map"></div>
+
+  <b-container class="bv-example-row">
+  <b-row>
+    <b-col sm="3">
+      <b-card
+          header="Search Results"
+          style="max-width: 20rem; max-heigt: 600px !important;"
+          class="mb-2 widen shadow">
+          <div class="scroller">
+            <b-list-group v-for="place in places" :key="place.id" class="left"> 
+              <b-list-group-item @click="updateSelected(place)" class="no-borders">
+                <div>
+                  <h5 class="mb-1">{{place.name}}</h5>
+                </div>
+                <p class="mb-1">{{place.vicinity}}</p>
+              </b-list-group-item>
+            </b-list-group>
+          </div>
+        </b-card>
+      
+    </b-col>
+    <b-col sm="6">
+      <div id="map" ref="map"></div>
+    </b-col>
+    <b-col sm="3">
+      <div v-if="showDetails"  class="left shadow">
+        <b-card
+          header="Venue Details"
+          :title="selectedPlace.name"
+          :sub-title="selectedPlace.vicinity"
+          style="max-width: 20rem;"
+          class="mb-2">
+          <hr />
+          <b-card-text>
+            <b>Average User Rating:</b> {{selectedPlace.rating}}
+            <br />
+            <b>Total User Ratings:</b> {{selectedPlace.user_ratings_total}}
+          </b-card-text>
+        </b-card>
+      </div>
+      
+      <div v-if="showDetails" style="margin-top: 35px;" class="shadow">
+        <b-card
+          header="TDP Reviews"
+          style="max-width: 20rem;"
+          class="mb-2">
+          
+          <b-card-text class="left">
+            <p><b>Natalie S:</b> "This place is great! I had a lot of fun!"</p>
+            <hr />
+            <p><b>Zachary L:</b> "Amazing vibes, I love it here."</p>
+            <hr />
+            <p><b>Anna C:</b> "I'm definitely bringing my friends here next time!!"</p>
+            <hr />
+          </b-card-text>
+        </b-card>
+      </div>
+
+    </b-col>
+  </b-row>
+</b-container>
+    
+    
   </div>
 </template>
 
@@ -10,15 +72,16 @@ import {gmapApi} from 'vue2-google-maps'
 import axios from "axios";
 
 export default {
-  name: 'HelloWorld',
+  name: 'Results',
   data(){
     return{
       center: { lat: 39.739071, lng: -75.539787 },
       lat: 39.739071,
       lng: -75.539787,
       type: "restaurant",
-      radius: 5,
       places: [],
+      showDetails: false,
+      selectedPlace: {},
       map: null,
     }
   },
@@ -35,7 +98,7 @@ export default {
   methods:{
         findCloseBuyButtonPressed() {
             const URL = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${
-                this.lat},${this.lng}&type=${this.type}&radius=${this.radius * 1000}&key=AIzaSyBNqBOnwbtk78eyzBLpNF0tTocn8MSI1-o`;
+                this.lat},${this.lng}&type=${this.type}&radius=5000&key=AIzaSyBNqBOnwbtk78eyzBLpNF0tTocn8MSI1-o`;
             axios.get(URL).then(response => {
                 this.places = response.data.results;
                 console.log(response.data.results);
@@ -68,7 +131,10 @@ export default {
                 });
             });
         },
-        
+        updateSelected (selectedItem) {
+        this.selectedPlace = selectedItem;
+        this.showDetails = true;
+     }
     }
 }
 </script>
@@ -88,6 +154,38 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.card-header{
+  text-align: center !important;
+}
+
+.left{
+  text-align: left;
+}
+
+.widen .card-body{
+  padding: 0;
+}
+
+.no-borders{
+  border-top: 0;
+  border-right: 0;
+  border-left: 0;
+  border-bottom: 1px solid rgb(63, 61, 61);
+  border-radius: 0%;
+}
+
+.dark-borders{
+  border: 1px solid rgb(63, 61, 61);
+  
+}
+
+.scroller{
+  max-height: 535px;
+  margin-bottom: 10px;
+  overflow:scroll;
+  overflow-x: hidden;
 }
 
 #map{
